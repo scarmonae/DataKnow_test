4701a86e14ceaf0394887ae824f232ad9bf8e4f6
scarmonae <scarmonae@eafit.edu.co>
2024-02-04 21:52:59 -0500

    Punto uno, actualziación

--- prueba_tecnica_sce.py
+++ prueba_tecnica_sce.py
@@ -3,13 +3,10 @@ DataKnow Technical test
 
 autor: Sebastian Carmona Estrada
 """
-import pdb
 import numpy as np
 import pandas as pd
 
 def custom_parser(file_path):
-    data = []
-
     with open(file_path, 'r', encoding='utf-8') as file:
         temp_dict = {}
         temp_dict["Agente"] = []
@@ -20,19 +17,10 @@ def custom_parser(file_path):
         for row in file:
             if not row.isspace():
                 if row.find("AGENTE") != -1:
-                    # temp_dict = {}
-                    # temp_dict["Agente"] = []
-                    # temp_dict["Planta"] = []
-                    # for h in np.arange(1,25):
-                    #     temp_dict["Hora_{h}".format(h=h)] = []
-
-                    # pdb.set_trace()
                     agent = row.split(":")[1].strip()
             
                 elif 'agent' in locals():
                     if row.split(",")[1].strip() == 'D':
-                        # pdb.set_trace()
-                        
                         temp_dict["Agente"].append(agent)
                         temp_dict["Planta"].append(row.split(',')[0].strip())
 
@@ -42,14 +30,10 @@ def custom_parser(file_path):
         
         return pd.DataFrame(temp_dict)
 
-
-
-
 def main():
     df = custom_parser(r"C:\Users\USUARIO\OneDrive - Universidad EAFIT\REPOSITORIOS\Test_Dataknow\Prueba_Tecnica\Datos3\OFEI1204.txt")
 
     print(df)
-
 
 if __name__ == "__main__":
     main()
